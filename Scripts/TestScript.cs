a957e04f79d6803bf8875775d01424051764429c
Digx7 <37136301+Digx7@users.noreply.github.com>
2023-05-30 21:24:29 -0700

    Got a custom logging system working

---
+++ Scripts/TestScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : CustomMonoBehaviorWrapper
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Log("Hello World");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
