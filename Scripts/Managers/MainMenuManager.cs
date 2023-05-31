2f1178b4dd8bdda6565a63a9ed218c0c78e52ac0
Digx7 <37136301+Digx7@users.noreply.github.com>
2023-05-31 09:55:12 -0700

    Got the quit and credits menu working

--- Scripts/Managers/MainMenuManager.cs
+++ Scripts/Managers/MainMenuManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using UnityEngine.Events;
 
 public class MainMenuManager : Singleton<MainMenuManager>
 {
@@ -10,54 +11,78 @@ public class MainMenuManager : Singleton<MainMenuManager>
     public MainMenuStates currentState;
 
     public List<MenuScreen> menuScreens;
-    private MenuScreen findScreenForState(MainMenuStates state)
+    private MenuScreen findScreenByState(MainMenuStates state)
     {
         return menuScreens.Find(item => item.currentState == state);
     }
+    private MenuScreen findScreenByName(string name)
+    {
+        return menuScreens.Find(screen => screen.name == name);
+    }
 
     public EventSystem eventSystem;
+    
+    private MenuScreen currentScreen;
 
-    public void OnConinuteClick()
+    protected override void Awake()
     {
-
+        base.Awake();
+        Initialize();
     }
 
-    public void OnNewGameClick()
+    private void Initialize()
     {
-
+        currentScreen = findScreenByState(currentState);
+        currentScreen.rootObject.SetActive(true);
     }
 
-    public void OnLoadGameClick()
+    public void OnSelect (string newState)
     {
-
+        OnSelect(findScreenByName(newState).currentState);
     }
 
-    public void OnOptionsClick()
+    public void OnSelect(MainMenuStates newState)
     {
-
+        OnChangeToState(newState, findScreenByState(newState).selectedButtonOnStateEnter);
     }
 
-    public void OnCreditsClick()
+    public void OnBack()
     {
-
+        OnChangeToState(currentScreen.exitToState, currentScreen.selectedButtonOnStateExit);
     }
 
-    public void OnQuitClick()
+    private void OnChangeToState(MainMenuStates newState, Button selectedButton)
     {
+        MenuScreen newScreen = findScreenByState(newState);
+        currentState = newState;
 
+        currentScreen.rootObject.SetActive(false);
+        currentScreen.OnExit.Invoke();
+        currentScreen = newScreen;
+        currentScreen.rootObject.SetActive(true);
+        currentScreen.OnEnter.Invoke();
+
+        eventSystem.SetSelectedGameObject(null);
+        eventSystem.SetSelectedGameObject(selectedButton.gameObject);
     }
+
+
 }
 
 [System.Serializable]
 public struct MenuScreen
 {
+    public GameObject rootObject;
+    public string name;
     public MainMenuStates currentState;
     public MainMenuStates exitToState;
     public Button selectedButtonOnStateEnter;
     public Button selectedButtonOnStateExit;
+    public UnityEvent OnEnter;
+    public UnityEvent OnExit;
 }
 
 public enum MainMenuStates
     {
-        MainMenu, NewGame, LoadGame, Options, Credits, Quit
+        MainMenu = 0, NewGame = 1, LoadGame = 2, Options = 3, Credits = 4, Quit = 5
     }
